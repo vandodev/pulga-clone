1c9809ae3a8ce03a82b9b59b676e9ec028bbc0a5
Evandro oliveira <vandoaparecido@hotmail.com>
2022-07-26 20:39:50 -0300

    :zap: Scooter :zap: Navegação e carregamento das fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
+import { useFonts } from "expo-font";
 import App from "./src";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/styles/theme";
+import { fonts } from "./src/styles/fonts";
 
 export default function Main() {
-  return <App />;
+  const [fontsLoaded] = useFonts(fonts);
+
+  if (!fontsLoaded) return null;
+  return (
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  );
 }
