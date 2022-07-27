336a54ee2c74abfdec807afac28ddeb2f1a62bf4
Evandro oliveira <vandoaparecido@hotmail.com>
2022-07-27 20:39:12 -0300

    :zap: Scooter :zap: Estrutura e navegação tela Home

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { OnBoarding } from "../pages/OnBoarding";
+import { Home } from "../pages/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,7 @@ const Navigation = () => {
       }}
     >
       <Stack.Screen name="onboarding" component={OnBoarding} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
 };
